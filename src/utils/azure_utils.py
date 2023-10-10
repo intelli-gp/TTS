f0544362d21ef41a6aa486da1295cda6076ffb4d
ahmedelsayed968 <ahmedeltop968@gmail.com>
2023-10-10 20:19:21 +0300

    feat: add azure storage class

--- src/utils/azure_utils.py
+++ src/utils/azure_utils.py
@@ -1,57 +1,69 @@
+from typing import AnyStr, Iterable, IO
+
 from azure.storage.blob import BlobServiceClient
 
 
-# def store_to_blob():
-#     # Replace with your Azure Storage account name and access key
-#     account_name = 'graduationwork3664469496'
-#     account_key = 'Vs7mgSGdtSV9S7c+CU8iRoFW74/QbqhHtsQuSudKKdE5sRsiU0k94vjFwOIlxRS79v0EsBxvQP0z+AStsanD+Q=='
-#
-#     # Create a BlockBlobService client
-#     blob_service = BlobServiceClient(account_name=account_name, account_key=account_key)
-#
-#     # Specify the container and blob name
-#     container_name = 'test-data-storage'
-#     blob_name = 'from-colab'
-#
-#     # Specify the data you want to upload
-#     data_to_upload = b'Hello, Azure Blob Storage!'
-#
-#     # Upload the data to the specified blob
-#     blob_service.create_blob_from_bytes(container_name, blob_name, data_to_upload)
-#
-#     print(f'Data uploaded to blob: {blob_name} in container: {container_name}')
+class AzureStorage(object):
+    def __init__(self,
+                 account_name: str,
+                 account_key: str,
+                 ):
+        self.account_name = account_name
+        self.account_key = account_key
+        self.blob_service_client = BlobServiceClient(account_url=f"https://{self.account_name}.blob.core.windows.net",
+                                                     credential=self.account_key)
+
+    def store(self,
+              container_name: str,
+              blob_name: str,
+              data_to_store: bytes | str | Iterable[AnyStr] | IO[AnyStr]) -> bool:
+        try:
+            container_client = self.blob_service_client.get_container_client(container_name)
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_client.upload_blob(data_to_store, overwrite=True)
+            return True
+        except Exception as E:
+            print(str(E))
+            return False
+
+    def delete(self,
+               container_name: str,
+               blob_name: str,
+               ) -> bool:
+        try:
+            container_client = self.blob_service_client.get_container_client(container_name)
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_client.delete_blob()
+            return True
+        except Exception as E:
+            print(str(E))
+            return False
+
+    def list_all_blobs(self, container_name):
+        blobs = []
+        container_client = self.blob_service_client.get_container_client(container_name)
+        for blob in container_client.list_blobs():
+            blobs.append(blob)
+        return blobs
+
+    def retrieve(self,
+                 container_name: str,
+                 blob_name: str,
+                 ) -> bytes | None:
+        try:
+            container_client = self.blob_service_client.get_container_client(container_name)
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_data = blob_client.download_blob()
+            data_bytes = blob_data.readall()
+            return data_bytes
+        except Exception as E:
+            print(str(E))
+            return None
 
 
-def store_blob_v2():
-    # from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-    # Replace with your Azure Storage account name and access key
-    account_name = 'graduationwork3664469496'
-    account_key = 'Vs7mgSGdtSV9S7c+CU8iRoFW74/QbqhHtsQuSudKKdE5sRsiU0k94vjFwOIlxRS79v0EsBxvQP0z+AStsanD+Q=='
-
-
-    # Create a BlobServiceClient
-    blob_service_client = BlobServiceClient(account_url=f"https://{account_name}.blob.core.windows.net",
-                                            credential=account_key)
-
-    # Specify the container name
-    container_name = 'test-data-storage'
-
-    # Specify the blob name
-    blob_name = 'from-local'
-
-    # Specify the data you want to upload
-    data_to_upload = b'Hello, Azure Blob Storage!'
-
-    # Get a reference to the container
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # Upload the data to the specified blob
-    blob_client = container_client.get_blob_client(blob_name)
-    blob_client.upload_blob(data_to_upload, overwrite=True)
-
-    print(f'Data uploaded to blob: {blob_name} in container: {container_name}')
 
 
 if __name__ == '__main__':
-    store_blob_v2()
+    pass
+

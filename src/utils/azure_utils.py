cc07db8b0917da26c1ba90fbe8e084d9d64624d4
ahmedelsayed968 <ahmedeltop968@gmail.com>
2023-10-12 02:10:19 +0300

    fix: handle storing large data

--- src/utils/azure_utils.py
+++ src/utils/azure_utils.py
@@ -1,5 +1,7 @@
 from typing import AnyStr, Iterable, IO
 import logging
+
+from azure.core.exceptions import HttpResponseError
 from azure.storage.blob import BlobServiceClient
 
 logging.basicConfig(filename='azure_util.log',
@@ -20,7 +22,14 @@ class AzureStorage(object):
     def store(self,
               container_name: str,
               blob_name: str,
-              data_to_store: bytes | str | Iterable[AnyStr] | IO[AnyStr]) -> bool:
+              data_to_store: bytes | str | Iterable[AnyStr] | IO[AnyStr],
+              chunk_size=4 * 1024 * 1024) -> bool:
+
+        global blob_client
+        data_size = len(data_to_store)
+        chunks = [data_to_store[i:i + chunk_size] for i in range(0, data_size, chunk_size)]
+        max_retries = 3
+        retries = 0
         try:
             logging.info(f'Start Storing at {container_name}....')
             container_client = self.blob_service_client.get_container_client(container_name)
@@ -28,9 +37,16 @@ class AzureStorage(object):
             blob_client.upload_blob(data_to_store, overwrite=True)
             logging.info(f'Storing operation completed!')
             return True
-        except Exception as E:
-            logging.exception(str(E))
-
+        except HttpResponseError as e:
+            logging.exception(f'Initial upload attempt failed with error: {e}')
+            while retries < max_retries:
+                try:
+                    for chunk in chunks:
+                        blob_client.upload_blob(chunk, overwrite=False)
+                    return True
+                except HttpResponseError as e:
+                    logging.exception(f'Upload attempt {retries + 1} failed with error: {e}')
+                    retries += 1
             return False
 
     def delete(self,
@@ -48,7 +64,7 @@ class AzureStorage(object):
             logging.exception(str(E))
             return False
 
-    def list_all_blobs(self, container_name:str) -> list | None:
+    def list_all_blobs(self, container_name: str) -> list | None:
         blobs = []
         try:
             container_client = self.blob_service_client.get_container_client(container_name)
